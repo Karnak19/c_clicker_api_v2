054fdec0f95565d9bd5ae19cb00a071a615cbd77
Gagnant Nicolas <gagnantnicolas64@gmail.com>
2019-04-24 11:44:02 +0200

    SQL filter by score and limit 10 on index.js on ligne 31

--- index.js
+++ index.js
@@ -19,56 +19,71 @@ const connection = mysql.createConnection(config);
 app.use(bodyparser.json());
 
 connection.connect(err => {
-   if (!err) console.log("DB connection success");
-   else console.log("DB connection failed" + err);
+  if (!err) console.log("DB connection success");
+  else console.log("DB connection failed" + err);
 });
 
 app.listen(port, () => console.log("Express server started"));
 
 //get all users
 app.get("/user", (req, res) => {
-   connection.query("SELECT * FROM Users", (err, rows, field) => {
+  connection.query(
+    "SELECT * FROM Users ORDER BY score DESC LIMIT 10",
+    (err, rows, field) => {
       if (!err) res.send(rows);
       else console.log(err);
-   });
+    }
+  );
 });
 
 // Get an user
 app.get("/user/:id", (req, res) => {
-   connection.query("SELECT * FROM Users WHERE id=?", [req.params.id], (err, rows, field) => {
+  connection.query(
+    "SELECT * FROM Users WHERE id=?",
+    [req.params.id],
+    (err, rows, field) => {
       if (!err) res.send(rows);
       else console.log(err);
-   });
+    }
+  );
 });
 
 // Add an user
 app.post("/user/subscribe", (req, res) => {
-   let pseudo = req.body.pseudo;
-   let isLogged = false;
-   let insert = { id: null, pseudo: pseudo, score: 0, isLogged: isLogged };
-   let sql = "INSERT INTO Users SET ?";
-   connection.query(sql, [insert], (err, rows, field) => {
-      if (!err) {
-         res.send(rows);
-         console.log(`User ${pseudo} successfully created`);
-      } else console.log(err);
-   });
+  let pseudo = req.body.pseudo;
+  let isLogged = false;
+  let insert = { id: null, pseudo: pseudo, score: 0, isLogged: isLogged };
+  let sql = "INSERT INTO Users SET ?";
+  connection.query(sql, [insert], (err, rows, field) => {
+    if (!err) {
+      res.send(rows);
+      console.log(`User ${pseudo} successfully created`);
+    } else console.log(err);
+  });
 });
 
 //Increment user score
 app.put("/user/:id/click", (req, res) => {
-   connection.query("UPDATE Users SET score=score+1 WHERE id=?", [req.params.id], (err, rows, field) => {
+  connection.query(
+    "UPDATE Users SET score=score+1 WHERE id=?",
+    [req.params.id],
+    (err, rows, field) => {
       if (!err) {
-         res.send(rows);
-         console.log(`${req.params.id} click successfully`);
+        res.send(rows);
+        console.log(`${req.params.id} click successfully`);
       } else console.log(err);
-   });
+    }
+  );
 });
 
 //Delete a user
 app.delete("/user/:id", (req, res) => {
-   connection.query("DELETE FROM Users WHERE id=?", [req.params.id], (err, rows, field) => {
+  connection.query(
+    "DELETE FROM Users WHERE id=?",
+    [req.params.id],
+    (err, rows, field) => {
       if (!err) res.send("Deleted successfully");
       else console.log(err);
-   });
+    }
+  );
 });
